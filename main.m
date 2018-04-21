1f26ebef4d3137931c1ea1c7b07b5340c0f64655
Carlos Patiño <carpat@microsoft.com>
2018-04-21 02:13:17 -0400

    Iterating..

--- main.m
+++ main.m
@@ -1,6 +1,19 @@
-clear
-clc
+%% main
+% This script calculates the Link Budget margin for the communications link
+% between:
+%   1. a radio unit and a repeater
+%   2. a dispatch center and a repeater
+%
+% The diagram for a generalized model of the network is located here:
+% 
 
+%% Clear everything
+clc
+clear all
+close all
+
+%% Architecture Enumeration
+% This script calculates the Link Margin for a variety of architectures.
 testCase = calculateEbNoTester;
 res = run(testCase)
 
