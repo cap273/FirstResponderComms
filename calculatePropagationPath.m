528810e85c168fa14397fb1375106636791520eb
Carlos Patiño <carpat@microsoft.com>
2018-05-06 21:46:25 -0400

    Adding function for propagation path

---
+++ calculatePropagationPath.m
@@ -0,0 +1,24 @@
+function s2 = calculatePropagationPath(s1,v1,v2)
+%calculatePropagationPath - Calculate the propagation path through
+%smoke/fire or through foliage.
+%
+%   Model for calculations:
+%       https://github.com/cap273/FirstResponderComms/blob/master/propagationPathCalculation.png
+%   
+%   OUTPUTS:
+%       s2: the propagation path through fire/smoke or through
+%       foliage, in meters
+%   INPUTS:
+%       s1: the slant range from the spoke to the hub/repeater, in meters
+%       v1: the height from the spoke to the hub/repeater, in meters
+%       v2: the maximum height of the cover due to foliage, smoke, or fire,
+%               in meters
+
+% Find the sine of the triangle
+sinTheta = v1/s1;
+
+% Find propagation path, as both triangles are similar to each other
+s2 = v2/sinTheta;
+
+end
+
